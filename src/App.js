7b1e8bab968f46be82d0cba81c77ce610d8b9282
NightSea <sweetmilkshake0324@gmail.com>
2022-12-23 12:01:14 +0800

    feat: RWD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import useRWD from "./hooks/useRWD";
 
 function App() {
   const [timerStr, setTimerStr] = useState("");
+  const device = useRWD();
 
   useEffect(() => {
     setTimerStr(getCount());
@@ -12,21 +14,34 @@ function App() {
     return () => {
       clearInterval(countDownTimer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getCount = () => {
     const retiredDate = new Date(2023, 3, 5, 18, 0, 0);
     const nowDate = new Date();
     const divTime = retiredDate - nowDate;
+    if (divTime <= 0) return "退伍啦！";
     let leftDay = Math.floor(divTime / 1000 / 60 / 60 / 24);
     let leftHour = Math.floor((divTime / 1000 / 60 / 60) % 24);
     let leftMinute = Math.floor((divTime / 1000 / 60) % 60);
     let leftSec = Math.floor((divTime / 1000) % 60);
+
     leftHour = leftHour < 10 ? `0${leftHour}` : leftHour;
     leftDay = leftDay < 10 ? `0${leftDay}` : leftDay;
     leftMinute = leftMinute < 10 ? `0${leftMinute}` : leftMinute;
     leftSec = leftSec < 10 ? `0${leftSec}` : leftSec;
-    return `剩餘 ${leftDay} 天  ${leftHour} 小時 ${leftMinute} 分鐘 ${leftSec} 秒`;
+
+    switch (device) {
+      case "mobile":
+        return `剩餘
+    ${leftDay} 天 ${leftHour} 小時
+    ${leftMinute} 分鐘 ${leftSec} 秒`;
+      case "tablet":
+      case "desktop":
+      default:
+        return `剩餘 ${leftDay} 天  ${leftHour} 小時 ${leftMinute} 分鐘 ${leftSec} 秒`;
+    }
   };
 
   return (
